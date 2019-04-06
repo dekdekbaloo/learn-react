1fd4b31249839361dc35604c106eea8f940361fa
Jessada Trirongkit <jessada.trirongkit@gmail.com>
2019-04-06 14:37:14 +0700

    Updates

---
+++ precache-manifest.cb35a537e4c8a23dc952815c6cd698d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/learn-react/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1f5a4aa29338276e39d8",
+    "url": "/learn-react/static/js/runtime~main.1bcabeff.js"
+  },
+  {
+    "revision": "92cfc1271ef61317a45c",
+    "url": "/learn-react/static/js/main.b7afb32f.chunk.js"
+  },
+  {
+    "revision": "4073f2ed420b23be37c4",
+    "url": "/learn-react/static/js/2.748fd8af.chunk.js"
+  },
+  {
+    "revision": "92cfc1271ef61317a45c",
+    "url": "/learn-react/static/css/main.46d13ea5.chunk.css"
+  },
+  {
+    "revision": "6065eb8dafacdfcb85ec4b8629070d5c",
+    "url": "/learn-react/index.html"
+  }
+];
