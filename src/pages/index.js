4c03cd028959a996cf4e354fa7c788893872cd2e
Jessada Trirongkit <jessada.trirongkit@gmail.com>
2019-04-06 11:37:26 +0700

    Added router + index page

---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.css'
+
+const IndexPage = () => (
+  <div className='index-page'>
+    <h1>มาเรียนรู้ React กันเถอะ</h1>
+  </div>
+)
+
+export default IndexPage
